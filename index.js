77455f6b372b6861c360b1f5d83feb2f615e3070
vgapp.adm <vgapp.adm@gmail.com>
2024-09-20 19:08:11 +0000

    api

--- index.js
+++ index.js
@@ -1,65 +1,62 @@
 var express = require("express");
 var geolib = require("geolib");
+var cors = require("cors")
+
 var app = express();
 var port = 5050;
 
+app.use(cors());
 
 var verdao_lojas_coords = [
     {
-        "latitude":-15.607282, 
-        "longitude":-56.119839
+        "latitude": -15.607282,
+        "longitude": -56.119839
     },
     {
-        "latitude":-15.612847, 
-        "longitude":-56.050579
+        "latitude": -15.612847,
+        "longitude": -56.050579
     },
     {
-        "latitude":-15.592621,
-        "longitude":-56.069962
+        "latitude": -15.592621,
+        "longitude": -56.069962
     },
     {
-        "latitude":-15.612804, 
-        "longitude":-56.050461
+        "latitude": -15.612804,
+        "longitude": -56.050461
     }
 ]
 
-app.use(express.static(__dirname + "/src/"));
-
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/src/index.html");
-});
-
-app.get("/geolocation/:lat/:log/:accuracy",(req,res) => {
+app.get("/geolocation/:lat/:log/:accuracy", (req, res) => {
 
     res.set({
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "POST, GET",
-    "Access-Control-Allow-Headers": "*",
-    "Access-Control-Max-Age": "86400"
-  });
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "POST, GET",
+        "Access-Control-Allow-Headers": "*",
+        "Access-Control-Max-Age": "86400"
+    });
 
-    
-    let coords = {"latitude" : req.params.lat, "longitude" : req.params.log};
+
+    let coords = { "latitude": req.params.lat, "longitude": req.params.log };
     let distances = [];
     const menor_distance_coords = [];
     let menor_distancia = 0;
     verdao_lojas_coords.forEach(item => {
 
-        distances.push(geolib.getDistance(coords, item,req.params.accuracy) / 1000);
+        distances.push(geolib.getDistance(coords, item, req.params.accuracy) / 1000);
     });
-    menor_distancia = distances.reduce((a,b) => Math.min(a,b));
-    verdao_lojas_coords.forEach(item => { 
-        if((geolib.getDistance(coords, item, req.params.accuracy)/1000) == menor_distancia){
+    menor_distancia = distances.reduce((a, b) => Math.min(a, b));
+    verdao_lojas_coords.forEach(item => {
+        if ((geolib.getDistance(coords, item, req.params.accuracy) / 1000) == menor_distancia) {
             menor_distance_coords.push(item);
         } else {
             console.log("adress not found : ", item);
         }
     });
-    
+
     res.json(
         {
-            lower_distance_coords:menor_distance_coords[0],
-            lower_distance:menor_distancia
+            lower_distance_coords: menor_distance_coords[0],
+            lower_distance: menor_distancia
         }
     );
 });
